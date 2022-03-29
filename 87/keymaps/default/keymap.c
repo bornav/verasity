f1e29cfdfeb5d0c6260a25b7e56b109c3d1e096a
bornav <borna.vincek1@gmail.com>
2022-03-29 15:47:20 +0200

    rnd

--- 87/keymaps/default/keymap.c
+++ 87/keymaps/default/keymap.c
@@ -16,7 +16,7 @@
 
 #include QMK_KEYBOARD_H
 
-#include "quantum/keymap_extras/keymap_croatian.h"
+//#include "quantum/keymap_extras/keymap_croatian.h"
 
 #define KC_MEN KC_MEDIA_PREV_TRACK
 #define KC_MES KC_MEDIA_PLAY_PAUSE
@@ -37,12 +37,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, MO(_FN), KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN] = LAYOUT_tkl_ansi(
-        RESET,      KC_MUTE, KC_VOLD, KC_VOLU, KC_F4,   KC_MEN,  KC_MES,  KC_MEP,  KC_F8,   KC_F9,   KC_F10,  KC_F11,       KC_F12,                                    KC_PSCR, NK_ON,   NK_OFF,
+        RESET,      KC_MUTE, KC_VOLU, KC_VOLD, KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_MEN,  KC_MES,      KC_MEP,                                    KC_PSCR, NK_ON,   NK_OFF,
         KC_NUMLOCK, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_MINUS,  KC_KP_PLUS,         KC_BSPC,               KC_F13,  KC_F14, KC_F15,
-        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_KP_1, KC_KP_2,      KC_KP_3,        KC_NONUS_BSLASH,           KC_F16,  KC_F17, KC_F18,
+        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_KP_1, KC_KP_2,      KC_KP_3,        KC_NONUS_BSLASH,           KC_F16,  KC_F17, EEPROM_RESET,
         KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_KP_4, KC_KP_5, KC_KP_6,      KC_KP_ENTER,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_KP_7, KC_KP_8, KC_KP_9, KC_RSFT,                                                          KC_BRIGHTNESS_UP,
-        KC_LCTL,    KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI,      MO(_FN), KC_RCTL,                          KC_LEFT, KC_BRIGHTNESS_DOWN, KC_RGHT
+        KC_LCTL,    AG_NORM, KC_LALT,                            KC_SPC,                             KC_RALT, AG_SWAP,      MO(_FN), KC_RCTL,                          KC_LEFT, KC_BRIGHTNESS_DOWN, KC_RGHT
     ),
 };
 
